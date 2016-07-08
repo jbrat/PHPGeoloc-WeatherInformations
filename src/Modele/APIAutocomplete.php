b4a4cc83374cc9cef699bf8130b9e9a0a4a63b3b
BRAT Julien <bratjulien@gmail.com>
2016-07-08 11:38:48 +0200

    ajout d'une methode pour l'autocompletion des pays

---
+++ src/Modele/APIAutocomplete.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace geoMarketing\Modele;
+
+use Curl\Curl;
+use geoMarketing\Exception\AutoCompleteCountryException;
+
+/**
+ * Autocomplete API for Country, Region
+ */
+class APIAutocomplete
+{
+    private $apiCountrysURL = "https://restcountries.eu/rest/v1/all";
+    
+    function getApiCountrysURL() {
+        return $this->apiCountrysURL;
+    }
+
+    function setApiCountrysURL($apiCountrysURL) {
+        $this->apiCountrysURL = $apiCountrysURL;
+    }
+
+    /**
+     * Method to get all the countries for autocompletion
+     * 
+     * @return Array countries
+     * 
+     * @throws AutoCompleteCountryException
+     */
+    public function getAutocompleteCountrys()
+    {
+        $countrys = array();
+        
+        $curl = new Curl();
+        $result = $curl->get($this->apiCountrysURL);
+        
+        if($curl->httpStatusCode != 200) {
+            throw new AutoCompleteCountryException("The API country aren't accessible for this moment");
+        }
+        
+        foreach($result as $country) {
+            $countrys[trim($country->name)] = trim($country->name);
+        }
+        
+        return json_encode($countrys);
+    }
+    
+}
